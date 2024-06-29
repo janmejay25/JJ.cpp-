9983a37953db4d6a9108aaf498fa67573a67a65e
janmejay25 <janmjaypandya@gmail.com>
2024-06-29 16:07:28 +0530

    odd even

---
+++ basic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    cout << "enter number" << endl;
+    cin >> i;
+    if(i%2==0){
+        cout<< "number " <<i<< " is even"<<endl;
+
+    }else{
+         cout<<  "number " <<i<< " is odd"<<endl;
+
+    }
+
+    return 0;
+}
