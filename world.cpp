50180eb7ec69d9b8a93ba34aa32436064753ec56
janmejay25 <janmjaypandya@gmail.com>
2025-11-28 09:23:26 +0530

    basics

--- world.cpp
+++ world.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
+#include <iostream> //preprocessor directive 
 using namespace std;
+using std::cout;//only cout from std namespace
 
+namespace myspace{
+    void display(){
+        cout << "inside namespace mysppace";
+    }
+}
 int main(){
-    cout << "jj world ls" << endl;
-     cout << "output " << endl;
+    myspace::display();
+
+    cout << "hello world" << endl;
+    std::cout <<"hello direct w/o using namespace";
     return 0;
 }
