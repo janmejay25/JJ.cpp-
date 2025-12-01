67926329351a9cad7c081bc05a08fc9ede2e0fc2
janmejay25 <janmjaypandya@gmail.com>
2025-12-01 10:46:43 +0530

    var and const

---
+++ 01_varconst.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //variable declaration
+    int score;
+    score = 95;
+
+    //constant declaration
+    //value cant be changed
+    const int score1 = 100;
+
+    cout << score <<score1 <<endl;
+    return 0;
+}
