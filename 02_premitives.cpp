57890dd78e88cabc25504f2032be5f17bf38ae8a
janmejay25 <janmjaypandya@gmail.com>
2025-12-01 10:12:15 +0530

    dataTypes

---
+++ 02_premitives.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int teaLeaves = 50;
+    float WaterTemp = 80.5;
+    double priceOfTea = 99.99;
+    // last char is printer 'AB' B will print
+    char teaGrade = 'A';
+    // except 0 all consider to value 1(true)
+    bool isReady = true;
+
+    cout << isReady << endl;
+    cout << priceOfTea << endl;
+    cout << teaLeaves << endl;
+    cout << WaterTemp << endl;
+    cout << teaGrade << endl;
+    return 0;
+}
