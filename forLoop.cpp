51ae9072657afa8bf88e39987d07feae367b1f3d
janmejay25 <janmjaypandya@gmail.com>
2025-12-04 17:41:47 +0530

    typeOfForLoop

---
+++ forLoop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int arrayno[5]={10,20,30,40,50};
+   for(int elemarray:arrayno){
+        cout<<elemarray<<endl;
+   }
+    
+    return 0;
+}
